b8c5607c95ffdbd8168eab335db324fa4aaffbfe
surgeoftheages <surendhar.s@aurosociety.org>
2023-08-18 16:22:40 +0530

    all files up
    only index remaininf

---
+++ WritingsOfSATM/HymnToDurga-Tamil/mobile/javascript/bookmark_config.js
@@ -0,0 +1,1 @@
+﻿var ols=[{caption:"Hymn to durga - Tamil 01",page:"1",url:"",level:"1",children:[]},{caption:"Hymn to durga - Tamil 02",page:"2",url:"",level:"1",children:[]},{caption:"Hymn to durga - Tamil 03",page:"3",url:"",level:"1",children:[]},{caption:"Hymn to durga - Tamil 04",page:"4",url:"",level:"1",children:[]}];
